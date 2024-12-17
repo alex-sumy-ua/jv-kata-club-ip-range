fc95cbbc9fc804dc3d22c90985a1c4a765a6f68e
alex-sumy-ua <plachkovskyy@gmail.com>
2024-12-17 16:34:14 +0000

    mvp completed

--- src/main/java/org/kata5/Main.java
+++ src/main/java/org/kata5/Main.java
@@ -1,17 +1,30 @@
 package org.kata5;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        System.out.println(countAddresses("10.0.0.0", "10.0.0.50")); // Output: 50
+        System.out.println(countAddresses("10.0.0.0", "10.0.1.0"));  // Output: 256
+        System.out.println(countAddresses("20.0.0.10", "20.0.1.0")); // Output: 246
+
     }
+
+    public static long countAddresses(String startIP, String endIP) {
+        long start = ipToLong(startIP);
+        long end = ipToLong(endIP);
+        return end - start;
+    }
+
+    public static long ipToLong(String ipAddress) {
+        String[] octets = ipAddress.split("\\."); // Split by "."
+        long result = 0;
+
+        for (int i = 0; i < 4; i++) {
+            result = (result << 8) + Integer.parseInt(octets[i]);
+        }
+
+        return result;
+    }
+
 }
